62ae058f8769dbf9f6831d81b46fa8ce574f0a85
Jeff <Xavierjeff451@gmail.com>
2022-04-17 20:44:05 +0300

    commit

---
+++ src/app/repo.ts
@@ -0,0 +1,3 @@
+export class Repo {
+    constructor(public name:string,public fullName:string, public details:string, public updatedAt:Date, public htmlUrl:string,public cloneUrl:string, public language:string, public createdAt:Date ){}
+}
