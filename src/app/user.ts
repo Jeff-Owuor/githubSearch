4624165e4b8da35d5d84db9edebfd7901e8fa2dc
Jeff <Xavierjeff451@gmail.com>
2022-04-17 20:47:39 +0300

    commit

---
+++ src/app/user.ts
@@ -0,0 +1,3 @@
+export class User {
+    constructor(public bio:string, public publicRepos:number,public login:string, public avatarUrl:string, public createdAt:Date, public updatedAt:Date, public htmlUrl:string){}
+}
