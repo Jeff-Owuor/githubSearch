22b77c133efd034c68a9353f963e6464678ceb2f
Jeff <Xavierjeff451@gmail.com>
2022-04-20 00:22:27 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SearchRoutingModule } from './search/search-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { RepoHighlightDirective } from './repo-highlight.directive';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     SearchResultsComponent,
-    RepoHighlightDirective
+    RepoHighlightDirective,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    SearchRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
